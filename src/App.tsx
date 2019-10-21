3630d81d0a4ca25cd27da20c40dd8c2530d54a18
Giorgos Vasileiou <g.vasileiou@gmail.com>
2019-10-21 16:49:56 +0300

    Rough test with a naive feature flag implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import * as React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, SemanticWIDTHS } from 'semantic-ui-react';
 import AppHeader from './AppHeader';
 import Profile from './Profile';
 import 'semantic-ui-css/semantic.min.css';
 import { RepoTabs } from './Tabs';
 
+const enabledComponents = () => {
+  let apps: String[] | null = null;
+  if (apps !== null) {
+    return apps;
+  }
+
+  apps = [];
+  if (process.env.REACT_APP_ENABLE_MY_REPOS || false) {
+    apps = ["RepoTabs"]
+  }
+  return apps;
+}
+
+const isComponentEnabled = (name: string) => {
+  const components = enabledComponents();
+
+  return components.includes(name);
+}
+
 const App = () => {
+
+  let columnsLength: SemanticWIDTHS;
+
+  if (isComponentEnabled("RepoTabs")) {
+    columnsLength = 2
+  } else {
+    columnsLength = 1
+  }
+
   return (
     <div className="App">
       <Grid divided='vertically'>
@@ -14,10 +42,12 @@ const App = () => {
             <AppHeader />
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row columns={2}>
+        <Grid.Row columns={columnsLength}>
+        {isComponentEnabled("RepoTabs") &&  
         <Grid.Column width={2}>
           <Profile />
         </Grid.Column>
+        }
         <Grid.Column width={14}>
           <RepoTabs />
         </Grid.Column>
